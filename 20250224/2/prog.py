9694232dd9e48027289a5e7a137a11ff64e69385
ArtyomOsipchuk <artos4002@yandex.ru>
2025-02-26 15:52:53 +0300

    доработка разбора и обработки команды addmon <x> <y> <name> <hello> merged to добавление в обработку команды addmon проверки того, что <name> - имя "штатного" существа

---
+++ 20250224/2/prog.py
@@ -0,0 +1,43 @@
+import cowsay
+
+dungeon = [[0 for i in range(10)] for j in range(10)]
+pos = [0, 0]
+
+def encounter(y, x):
+    name, message = dungeon[y][x]
+    print(cowsay.cowsay(message))
+
+while (c := input()):
+    movement = True
+    c = str(c).split()
+    match c:
+        case ["up"]:
+            pos[1] = (pos[1] + 1) % 10
+            print(f"Moved to ({pos[0]}, {pos[1]})")  
+        case ["down"]:
+            pos[1] = (pos[1] - 1) % 10
+            print(f"Moved to ({pos[0]}, {pos[1]})") 
+        case ["right"]:
+            pos[0] = (pos[0] + 1) % 10
+            print(f"Moved to ({pos[0]}, {pos[1]})") 
+        case ["left"]:
+            pos[0] = (pos[0] - 1) % 10
+            print(f"Moved to ({pos[0]}, {pos[1]})") 
+        case ["addmon", x, y, name, hello]:
+            movement = False
+            if x.isdigit() and y.isdigit() and name in cowsay.list_cows():
+              y, x = int(y), int(x)
+              print(f"Added monster {name} to ({x}, {y}) saying {hello}")
+              if dungeon[y][x]:
+                  print("Replaced the old monster")
+              dungeon[y][x] = [name, hello]
+            else:
+              print("Invalid arguments")
+        case _:
+            movement = False
+            print("Invalid command")
+    if not movement:
+        continue
+    if dungeon[pos[1]][pos[0]]:
+        print(f"Moved to ...")
+        encounter(pos[1], pos[0])
