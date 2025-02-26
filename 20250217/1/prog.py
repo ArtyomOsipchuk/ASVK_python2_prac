bb652d7d4e8d9f6dbfbc21eb0753eb7cc0ee91b3
ArtyomOsipchuk <artos4002@yandex.ru>
2025-02-26 09:53:34 +0300

    Пройти по истории от последнего к начальному коммиту ветки, для каждого коммита вывести объект-дерево
    - В случае нескольких родительских коммитов выбирать первый, топологией не заморачиваться.
    - "TREE" большими буквами выделяет строку, где сказано, дерево для какого коммита выводится.

--- 20250217/1/prog.py
+++ 20250217/1/prog.py
@@ -10,34 +10,34 @@ if len(sys.argv) == 2:
         Id = basename(store)
         print(Id)
 elif len(sys.argv) == 3:
-    for head in Path(sys.argv[1]).glob(f".git/refs/heads/{sys.argv[2]}"):
+    for head in Path(sys.argv[1]).glob(f".git/logs/refs/heads/{sys.argv[2]}"):
         with open(head, "rb") as f:
-            com = f.read().decode()
-    for store in Path(sys.argv[1]).glob(".git/objects/??/*"):
-        #print("store", store)
-        Id = basename(dirname(store)) + basename(store)
-        if Id + "\n" == com:
-            with open(store, "rb") as f:
-                obj = zlib.decompress(f.read())
-                header, _, body = obj.partition(b'\x00')
-                kind, size = header.split()
-                tree = body.decode().split()[1]
-                print([tree])
-            break
-    for store in Path(sys.argv[1]).glob(".git/objects/??/*"):
-        Id = basename(dirname(store)) + basename(store)
-        if Id == tree:
-            with open(store, "rb") as f:
-                obj = zlib.decompress(f.read())
-                header, _, body = obj.partition(b'\x00')
-                kind, size = header.split()
-                out = body #.decode()
-                print(header, body)
-            if kind == b'tree':
-                tail = body
-                while tail:
-                    treeobj, _, tail = tail.partition(b'\x00')
-                    tmode, tname = treeobj.split()
-                    num, tail = tail[:20], tail[20:]
-                    print(f"blob {num.hex()}\t{tname.decode()}")
-            print(f"tree {Id}\t{sys.argv[2]}")
+            coms = f.read().decode().split("\n")[:-1]
+        for commit in coms:
+            com = commit.split()[1]
+            print("TREE for commit", com)
+            for store in Path(sys.argv[1]).glob(".git/objects/??/*"):
+                Id = basename(dirname(store)) + basename(store)
+                if Id == com:
+                    with open(store, "rb") as f:
+                        obj = zlib.decompress(f.read())
+                        header, _, body = obj.partition(b'\x00')
+                        kind, size = header.split()
+                        tree = body.decode().split()[1]
+                    break
+            for store in Path(sys.argv[1]).glob(".git/objects/??/*"):
+                Id = basename(dirname(store)) + basename(store)
+                if Id == tree:
+                    with open(store, "rb") as f:
+                        obj = zlib.decompress(f.read())
+                        header, _, body = obj.partition(b'\x00')
+                        kind, size = header.split()
+                        out = body
+                    if kind == b'tree':
+                        tail = body
+                        while tail:
+                            treeobj, _, tail = tail.partition(b'\x00')
+                            tmode, tname = treeobj.split()
+                            num, tail = tail[:20], tail[20:]
+                            print(f"blob {num.hex()}\t{tname.decode()}")
+                    print(f"tree {Id}\t{sys.argv[2]}")
