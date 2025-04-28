5ca7724391db134dc5f2c386ca828f1265abb516
ArtyomOsipchuk <artos4002@yandex.ru>
2025-04-28 14:09:33 +0300

    Stat with doit

--- 20250428/0/dodo.py
+++ 20250428/0/dodo.py
@@ -1,7 +1,10 @@
 from pathlib import Path
+from zipfile import ZipFile
 
 
 DOIT_CONFIG = {'default_tasks': ['docs']}
+DOCZIP = 'docs.zip'
+DOCLIST = 'docs.list'
 
 def task_docs():
     """Build documentation"""
@@ -19,5 +22,19 @@ def task_erase():
 def task_zip():
     """Zip builded html to docs.zip"""
     return {
-        'actions': ['zip -r docs.zip _build/html']
+        'actions': [f'zip -r {DOCZIP} _build/html']
+    }
+
+def zipper(outfile, infile):
+    with ZipFile(outfile) as outzip:
+        names = outzip.namelist()
+    with open(infile, "w") as inzip:
+        print("\n".join(names), file=inzip)
+
+def task_stat():
+    """Archive documentation"""
+    return {
+        'actions': [(zipper, [f'{DOCZIP}', f'{DOCLIST}'])],
+        'file_dep': [f'{DOCZIP}'],
+        'targets': [f'{DOCLIST}']
     }
