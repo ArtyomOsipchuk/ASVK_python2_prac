018ae2a52a6323a5ec67ebc8e7c5fc7e62251f25
ArtyomOsipchuk <artos4002@yandex.ru>
2025-04-28 13:29:18 +0300

    Doit erase + file_dep

--- 20250428/0/dodo.py
+++ 20250428/0/dodo.py
@@ -1,5 +1,15 @@
+pathlib import Path
+
+
 def task_docs():
     """Build docs with doit"""
     return {
+        'file_dep': list(Path(".").glob("*.{py, rst}"))
         'actions': ['sphinx-build -M html source _build']
+    }
+
+def task_erase():
+    """Erase smthg with doit"""
+    return {
+        'actions': ['git reset --hard', 'git clean -xdf']
     }
