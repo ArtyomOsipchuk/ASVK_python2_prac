791b020cdc2008c17786b729889d9fa06b4c6d88
ArtyomOsipchuk <artos4002@yandex.ru>
2025-04-28 13:42:23 +0300

    Zip doit added

--- 20250428/0/dodo.py
+++ 20250428/0/dodo.py
@@ -4,14 +4,20 @@ from pathlib import Path
 DOIT_CONFIG = {'default_tasks': ['docs']}
 
 def task_docs():
-    """Build docs with doit"""
+    """Build documentation"""
     return {
         'file_dep': [*Path(".").glob("*.py"), *Path(".").glob("*.rst")],
         'actions': ['sphinx-build -M html source _build']
     }
 
 def task_erase():
-    """Erase smthg with doit"""
+    """Erase generates and new files"""
     return {
         'actions': ['git reset --hard', 'git clean -xdf']
+    }
+
+def task_zip():
+    """Zip builded html to docs.zip"""
+    return {
+        'actions': ['zip -r docs.zip _build/html']
     }
