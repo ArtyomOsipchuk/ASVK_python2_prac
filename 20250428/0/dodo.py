7bf46577cec94878966da42218019f6a4c461be2
ArtyomOsipchuk <artos4002@yandex.ru>
2025-04-28 13:34:21 +0300

    Default task - docs and *{py,rst} to docs

--- 20250428/0/dodo.py
+++ 20250428/0/dodo.py
@@ -1,10 +1,12 @@
 from pathlib import Path
 
 
+DOIT_CONFIG = {'default_tasks': ['docs']}
+
 def task_docs():
     """Build docs with doit"""
     return {
-        'file_dep': list(Path(".").glob("*.{py, rst}"))
+        'file_dep': [*Path(".").glob("*.py"), *Path(".").glob("*.rst")],
         'actions': ['sphinx-build -M html source _build']
     }
 
